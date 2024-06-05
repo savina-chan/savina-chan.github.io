30f718d6a3a5a6f5a04673b816bdfa4ef3fde95b
Savina Chan <savinachan1582@gmail.com>
2024-06-05 15:54:34 -0400

    added fade animation to experience section

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { fadeInAnimation } from 'src/assets/animations/animation';
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.scss']
+  styleUrls: ['./experience.component.scss'],
+  animations: [fadeInAnimation]
 })
 export class ExperienceComponent {
 
