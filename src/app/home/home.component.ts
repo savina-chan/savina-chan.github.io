d307541b6511c09fd9bedc9f583c9a399c5713fc
Savina Chan <savinachan1582@gmail.com>
2024-06-05 15:53:21 -0400

    added fade animation to about and deleted irrelevant line from header

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { fadeInAnimation } from 'src/assets/animations/animation';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [fadeInAnimation]
 })
 export class HomeComponent {
 
