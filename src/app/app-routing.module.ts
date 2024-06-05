39791d6cddd0ace28e9b775d21825fa8abfe7d5f
Savina Chan <savinachan1582@gmail.com>
2024-06-05 12:13:01 -0400

    reset website and add home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { EducationComponent } from './education/education.component';
-import { ExperienceComponent } from './experience/experience.component';
 
-const routes: Routes = [{path:'app-home', component:HomeComponent, title:"Home"},
-{path:'app-education', component:EducationComponent, title:"Education"},
-{path:'app-experience', component: ExperienceComponent, title:"Experience"}];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { EducationComponent } from './education/education.component';
-import { ExperienceComponent } from './experience/experience.component';
-import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { IntroComponent } from './intro/intro.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { SmoothScrollDirective } from './smooth-scroll.directive'
+import { SmoothScrollDirective } from './smooth-scroll.directive';
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    EducationComponent,
-    ExperienceComponent,
-    FooterComponent,
-    NavbarComponent,
-    IntroComponent,
-    AboutMeComponent,
-    SmoothScrollDirective
+    SmoothScrollDirective,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
