84745824fa5378b5a5b8bafeaf86b76e6d9b5d9d
Savina Chan <savinachan1582@gmail.com>
2024-06-05 15:51:58 -0400

    created fade animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { fadeInAnimation } from 'src/assets/animations/animation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [fadeInAnimation]
 })
 export class AppComponent {
   title = 'portfolio';
