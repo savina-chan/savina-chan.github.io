be5a10597e95a6b5fd450e906a6fc5a335d1557e
Savina Chan <savinachan1582@gmail.com>
2024-06-06 15:51:47 -0400

    deployed to github pages

--- docs/assets/animations/animation.ts
+++ docs/assets/animations/animation.ts
@@ -3,6 +3,6 @@ import { trigger, transition, style, animate } from '@angular/animations';
 export const fadeInAnimation = trigger('fadeIn', [
   transition(':enter', [
     style({ opacity: 0 }),
-    animate('1.5s', style({ opacity: 1 }))
+    animate('0.8s', style({ opacity: 1 }))
   ]),
 ]);
